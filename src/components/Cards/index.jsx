3cfa5dc1a9a850fb49160f61e0f2a36e9e80b445
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-22 16:42:42 -0300

    Refactor: Corrige bug que impedia os preços de aparecerem

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,17 +1,18 @@
 const Cards = (props) => {
-    const NewPrice = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(parseFloat((props.products.newPrice / 100)).toFixed(2))
     return (
-        <div className="containerCards">
-            <div className="cards">
-                <img src={props.products.image} />
-            </div>
-            <div className="infoCards">
-                <p className="nameProduct">{props.products.name}</p>
-                <p className="infoProduct">{props.products.description}</p>
-                <p className="oldPrice">De:R$ {props.products.oldPrice}</p>
-                <p className="newPrice">Por:{parseInt(NewPrice)} </p>
-                <p className="oldPrice">ou {props.products.installments.count}x{props.products.installments.value}</p>
-                <button className="buttonBuy">Comprar</button>
+        <div className="teste">
+            <div className="container containerCards">
+                <div className="cards">
+                    <img src={props.products.image} />
+                </div>
+                <div className="infoCards">
+                    <p className="nameProduct">{props.products.name}</p>
+                    <p className="infoProduct">{props.products.description}</p>
+                    <p className="oldPrice">De:R$ {props.products.oldPrice},00</p>
+                    <p className="newPrice">Por:R${props.products.price},00 </p>
+                    <p className="oldPrice">ou {props.products.installments.count}x de: {props.products.installments.value}0</p>
+                    <button className="buttonBuy">Comprar</button>
+                </div>
             </div>
         </div>
 
