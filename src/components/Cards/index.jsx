bf973d3401727e57eaa4dc9e9f42bf03dd58972d
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-22 17:44:48 -0300

    fix: limpeza de código

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,20 +1,19 @@
 const Cards = (props) => {
     return (
-        <div className="teste">
-            <div className="container containerCards">
-                <div className="cards">
-                    <img src={props.products.image} />
-                </div>
-                <div className="infoCards">
-                    <p className="nameProduct">{props.products.name}</p>
-                    <p className="infoProduct">{props.products.description}</p>
-                    <p className="oldPrice">De:R$ {props.products.oldPrice},00</p>
-                    <p className="newPrice">Por:R${props.products.price},00 </p>
-                    <p className="oldPrice">ou {props.products.installments.count}x de: {props.products.installments.value}0</p>
-                    <button className="buttonBuy">Comprar</button>
-                </div>
+        <div className="container containerCards">
+            <div className="cards">
+                <img src={props.products.image} />
+            </div>
+            <div className="infoCards">
+                <p className="nameProduct">{props.products.name}</p>
+                <p className="infoProduct">{props.products.description}</p>
+                <p className="oldPrice">De:R$ {props.products.oldPrice},00</p>
+                <p className="newPrice">Por:R${props.products.price},00 </p>
+                <p className="oldPrice">ou {props.products.installments.count}x de: {props.products.installments.value}0</p>
+                <button className="buttonBuy">Comprar</button>
             </div>
         </div>
+
 
     )
 }
