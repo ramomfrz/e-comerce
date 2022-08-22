8e20244603fa400c9e3c8991703d10f48bd49869
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-22 02:55:19 -0300

    Build: cria componente de footer da página

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footerContainer">
+                <p>Testando suas habilidades em HTML, CSS e JS. <br />Linx Impulse <br />2019</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
