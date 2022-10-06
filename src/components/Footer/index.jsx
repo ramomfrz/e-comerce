b6b5525eff54ac07cea56548f9ce5ecd38001849
Ramom Ferraz <ramomferraz@hotmail.com>
2022-10-06 17:28:30 -0300

    refactor: alterações no footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="footerContainer">
-                <p>Testando suas habilidades em HTML, CSS e JS. <br />Linx Impulse <br />2019</p>
+                <p>Testando minhas habilidades em HTML, CSS e JS. <br />Ramom Ferraz <br />2022</p>
             </div>
         </footer>
     )
